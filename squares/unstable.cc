c5078e602873d1f1ca3f8149c1788ec3d5db1a64
Joshua Simpson <jwvsimps@uwaterloo.ca>
2014-11-29 20:12:28 -0500

    Added draw method for UnstableSquare.

--- squares/unstable.cc
+++ squares/unstable.cc
@@ -16,5 +16,17 @@ void UnstableSquare::remove(int count){
 
 void UnstableSquare::draw(Xwindow *window, int x, int y) {
    
-   window->fillRectangle(x, y, 50, 50, 1); 
+    int height = Settings::SQUARE_HEIGHT; // save us some typing
+    int width = Settings::SQUARE_WIDTH;
+
+    int ribbon_height = height / 5;
+    int ribbon_width = width / 5;
+
+    window->fillRectangle(x, y, width, height, 1);
+    window->fillRectangle(x + ribbon_width, y, 3 * ribbon_width, height,
+        getColorInt());
+    window->fillRectangle(x, y + ribbon_height, width, 3 * ribbon_height,
+        getColorInt());
+
+    window->drawString(x + (width / 2) - 3, y + (height / 2) + 3, "S", 1); 
 }
