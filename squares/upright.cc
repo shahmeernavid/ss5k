06a167760034bb9ae490d488b29048c5dc76d0f7
Joshua Simpson <jwvsimps@uwaterloo.ca>
2014-11-29 21:46:36 -0500

    Changed text colour to black for all squares and changed letters to make more sense.

--- squares/upright.cc
+++ squares/upright.cc
@@ -12,13 +12,13 @@ void UprightSquare::remove(int count){
 
 void UprightSquare::draw(Xwindow *window, int x, int y) {
    
-   int height = Settings::SQUARE_HEIGHT; // save some typing
-   int width = Settings::SQUARE_WIDTH;
-   int narrow_width = Settings::SQUARE_WIDTH / 5;
+    int height = Settings::SQUARE_HEIGHT; // save some typing
+    int width = Settings::SQUARE_WIDTH;
+    int narrow_width = Settings::SQUARE_WIDTH / 5;
 
-   window->fillRectangle(x, y, narrow_width, height, 8); 
-   window->fillRectangle(x + narrow_width, y, narrow_width * 3, height, getColorInt()); 
-   window->fillRectangle(x + 4 * narrow_width, y, narrow_width, height, 8); 
+    window->fillRectangle(x, y, narrow_width, height, 8); 
+    window->fillRectangle(x + narrow_width, y, narrow_width * 3, height, getColorInt()); 
+    window->fillRectangle(x + 4 * narrow_width, y, narrow_width, height, 8); 
 
-   window->drawString(x + (width / 2) - 5, y + (height / 2) + 5, "US", 9);
+    window->drawString(x + (width / 2) - 3, y + (height / 2) + 3, "U", 1); 
 }
