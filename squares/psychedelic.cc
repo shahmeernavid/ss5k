aeb2bdbc8c049523804db71daab60a3579c6d756
Joshua Simpson <jwvsimps@uwaterloo.ca>
2014-11-29 19:53:52 -0500

    Improved draw method for PsychedlicSquare.

--- squares/psychedelic.cc
+++ squares/psychedelic.cc
@@ -14,13 +14,21 @@ void PsychedelicSquare::draw(Xwindow *window, int x, int y) {
 
     int height = Settings::SQUARE_HEIGHT; // save us some typing
     int width = Settings::SQUARE_WIDTH;
+
+    int ribbon_height = height / 5;
+    int ribbon_width = width / 5;
    
     for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            window->fillRectangle(x + (i * width/5), y + (j * 10), 10, 10, rand() % 10); 
-        }
+        window->fillRectangle(x + (i * ribbon_width), y, ribbon_width, ribbon_height, rand() % 10); 
+        window->fillRectangle(x + (i * ribbon_width), y + 4 * ribbon_height, ribbon_width, ribbon_height, rand() % 10); 
     }
 
-    window->fillRectangle(x + 10, y + 10, 30, 30, getColorInt());
-    window->drawString(x + (width / 2) - 3, y + (height / 2) + 3, "P", 9); 
+    for (int i = 0; i < 5; ++i) {
+        window->fillRectangle(x, y + (i * ribbon_height), ribbon_width, ribbon_height, rand() % 10); 
+        window->fillRectangle(x + 4 * ribbon_height, y + (i * ribbon_height), ribbon_width, ribbon_height, rand() % 10); 
+    }
+
+    window->fillRectangle(x + ribbon_width, y + ribbon_height, 3 * ribbon_width,
+        3 * ribbon_height, getColorInt());
+    window->drawString(x + (width / 2) - 3, y + (height / 2) + 3, "P", 1); 
 }
